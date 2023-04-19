66ceb2422309b0dd532065df807b6b428a5f1d37
Hoang Phuc <phuc.nguyen2003@hcmut.edu.vn>
2023-04-19 19:37:33 +0700

    change homepage

--- views/admin/header.php
+++ views/admin/header.php
@@ -19,3 +19,24 @@
 	<!-- Theme style-->
 	<link rel="stylesheet" href="public/dist/css/adminlte.min.css">
 	<!-- Add Style -->
+	<!-- Favicon -->
+    <link href="./public/img/layouts/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="./public/js/layouts/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="./public/js/layouts/animate/animate.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="./public/css/layouts/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="./public/css/layouts/style.css" rel="stylesheet">
--- views/main/footer.php
+++ views/main/footer.php
@@ -16,21 +16,22 @@
                             </div>
                             <div class="d-flex mb-2">
                                 <i class="bi bi-geo-alt text-primary me-2"></i>
-                                <p class="mb-0">123 Street, New York, USA</p>
+                                <p class="mb-0">364 Đ. Cộng Hòa, Tân Bình, Thành phố Hồ Chí Minh, Việt Nam</p>
                             </div>
                             <div class="d-flex mb-2">
                                 <i class="bi bi-envelope-open text-primary me-2"></i>
-                                <p class="mb-0">info@example.com</p>
+                                <p class="mb-0">bosch-infoteam@vn.bosch.com</p>
                             </div>
                             <div class="d-flex mb-2">
                                 <i class="bi bi-telephone text-primary me-2"></i>
-                                <p class="mb-0">+012 345 67890</p>
+                                <p class="mb-0">(84 28) 6258 3690</p>
                             </div>
                             <div class="d-flex mt-4">
-                                <a class="btn btn-primary btn-square me-2" href="#"><i class="fab fa-twitter fw-normal"></i></a>
-                                <a class="btn btn-primary btn-square me-2" href="#"><i class="fab fa-facebook-f fw-normal"></i></a>
-                                <a class="btn btn-primary btn-square me-2" href="#"><i class="fab fa-linkedin-in fw-normal"></i></a>
-                                <a class="btn btn-primary btn-square" href="#"><i class="fab fa-instagram fw-normal"></i></a>
+                                <a class="btn btn-primary btn-square me-2" href="https://twitter.com/BoschGlobal"><i class="fab fa-twitter fw-normal"></i></a>
+                                <a class="btn btn-primary btn-square me-2" href="https://www.youtube.com/channel/UCOSpB-OqjIhG3nZ-dOzVVzQ"><i class="fab fa-youtube fw-normal"></i></a>
+                                <a class="btn btn-primary btn-square me-2" href="https://www.facebook.com/OfficialBoschVietnam"><i class="fab fa-facebook-f fw-normal"></i></a>
+                                <a class="btn btn-primary btn-square me-2" href="https://www.linkedin.com/company/bosch-asean/"><i class="fab fa-linkedin-in fw-normal"></i></a>
+                                <a class="btn btn-primary btn-square" href="https://www.instagram.com/boschglobal/"><i class="fab fa-instagram fw-normal"></i></a>
                             </div>
                         </div>
                         <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
@@ -38,12 +39,12 @@
                                 <h3 class="text-light mb-0">Popular Links</h3>
                             </div>
                             <div class="link-animated d-flex flex-column justify-content-start">
-                                <a class="text-light mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
-                                <a class="text-light mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Intro</a>
-                                <a class="text-light mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Achievements</a>
-                                <a class="text-light mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Services</a>
-                                <a class="text-light mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>News</a>
-                                <a class="text-light" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Connections</a>
+                                <a class="text-light mb-2" href="index.php?page=main&controller=layouts&action=index"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
+                                <a class="text-light mb-2" href="index.php?page=main&controller=about&action=index"><i class="bi bi-arrow-right text-primary me-2"></i>Intro</a>
+                                <a class="text-light mb-2" href="index.php?page=main&controller=archive&action=index"><i class="bi bi-arrow-right text-primary me-2"></i>Achievements</a>
+                                <a class="text-light mb-2" href="index.php?page=main&controller=services&action=index"><i class="bi bi-arrow-right text-primary me-2"></i>Services</a>
+                                <a class="text-light mb-2" href="index.php?page=main&controller=blog&action=index"><i class="bi bi-arrow-right text-primary me-2"></i>News</a>
+                                <a class="text-light" href="index.php?page=main&controller=contact&action=index"><i class="bi bi-arrow-right text-primary me-2"></i>Connections</a>
                             </div>
                         </div>
                     </div>
