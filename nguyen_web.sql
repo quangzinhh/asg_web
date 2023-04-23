d6b33a3516cab2b551ca74055c2bf884592799d2
Đặng Quang Vinh <97649829+quangzinhh@users.noreply.github.com>
2023-04-23 09:42:14 +0700

    Update nguyen_web.sql

--- nguyen_web.sql
+++ nguyen_web.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `web`
 --
+CREATE DATABASE IF NOT EXISTS `web` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `web`;
 
 -- --------------------------------------------------------
 
